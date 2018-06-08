fa954acf28e144c5dc3656c9d764819e89c7d8e1
afreen23 <afreenrahman78@gmail.com>
2018-06-08 13:08:51 +0530

    minor fixes

--- src/components/IntroducingIgnitus/introducingignitus.js
+++ src/components/IntroducingIgnitus/introducingignitus.js
@@ -6,12 +6,12 @@ const IntroducingIgnitus =() =>{
   return (
     <div className = "container-fluid main">
       <div className = "row d-flex img-1">
-        <img src={ui} alt="" className="col-sm-6 img-1" />
-        <div className = "col-sm-6 text-center d-flex flex-column flex-sm-grow-1 ">
-            <div className="mt-4 intro">Introducing</div>
-            <div className="mt-2 ignitus-color img-1 ign">Ignitus</div>
-            <div className="mt-4 ig-ur-re img-1">Ignite your Resume</div>
-            <div className="mt-4 mb-3 sky img-1 px-5">'Skyrocketing your career by providing you the best global opportunities.'</div>
+        <img src={ui} alt="" className="col-sm-6" />
+        <div className = "col-sm-6 mt-1 text-center d-flex flex-column flex-sm-grow-1 ">
+            <h1 className="mt-4 intro">Introducing</h1>
+            <div className=" ignitus-color ign">Ignitus</div>
+            <h4 className="mt-4 mb-1 ig-ur-re ">Ignite your Resume</h4>
+            <h5 className="mt-4 pt-md-2 sky px-md-5">'Skyrocketing your career by providing you the best global opportunities.'</h5>
         </div>
       </div>
     </div>
